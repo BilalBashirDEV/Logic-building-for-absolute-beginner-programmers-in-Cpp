3d45a331608e15cd9169dfc1a738b29a471e7ad0
Muhammad Bilal <2018CE247@student.uet.edu.pk>
2021-02-05 01:10:23 +0500

    Add files via upload

---
+++ days of week.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char x;
+	char a;
+	char b;
+	char c;
+	char d;
+	char e;
+	char f;
+	char g;
+	cout<<"enter a value"<<endl;
+	cin>>x;
+	if (x=='a')
+	{
+		cout<<"monday";
+	}
+	if (x=='b')
+	{
+		cout<<"tuesday";
+	}
+	if(x=='c')
+	{
+		cout<<"wednesday";
+	}
+
+    
+	
+ 	
+	return 0;
+}
